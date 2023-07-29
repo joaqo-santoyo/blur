502c3609631d83539ba95f87687bbe4df823726d
joaquin <joaquin.xyz@gmail.com>
2023-07-28 19:37:08 -0600

    Cleanup macos entry

--- src/main/main-mac.m
+++ src/main/main-mac.m
@@ -4,6 +4,21 @@
 #include "app.h"
 
 
+@interface OpenGLView : NSOpenGLView
+@end
+@implementation OpenGLView
+
+- (void) prepareOpenGL {
+    [super prepareOpenGL];
+    appInit();
+}
+
+- (void) drawRect: (NSRect) bounds {
+    NSLog(@"OpenGLView::drawRect\n");
+    appRender();
+    glSwapAPPLE();
+}
+@end
 
 @interface AppDelegate : NSObject<NSApplicationDelegate>
 @property (nonatomic, strong) NSWindow *window;
@@ -11,47 +26,33 @@
 @end
 @implementation AppDelegate
 - (void) applicationDidFinishLaunching:(NSNotification*) notification {
+    NSLog(@"AppDelegate::applicationDidFinishLaunching");
+    
     NSRect rect = NSMakeRect(0, 0, screenInfo.width, screenInfo.height);
+    
+    NSOpenGLPixelFormatAttribute attributes[] = {
+        NSOpenGLPFADoubleBuffer,
+        NSOpenGLPFADepthSize, 8,
+        NSOpenGLPFAColorSize, 32,
+        NSOpenGLPFAColorFloat,
+        NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersionLegacy,
+        0
+    };
+    NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attributes];
+    self.openGLView = [[OpenGLView alloc] initWithFrame:rect pixelFormat:pixelFormat];
+    
     self.window = [[NSWindow alloc]
        initWithContentRect:rect
        styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable
        backing:NSBackingStoreBuffered
        defer:NO
     ];
-    [self.window setTitle:@"Hello AppKit"];
+    [self.window setTitle:@"Blur"];
     [self.window cascadeTopLeftFromPoint:NSMakePoint(100,100)];
-
-//    NSTextView* textView = [[NSTextView alloc] initWithFrame:rect];
-//    [textView setString:@"Hello AppKit"];
-//    [self.window.contentView addSubview:textView];
-
-    NSOpenGLPixelFormatAttribute attributes[] = {
-        NSOpenGLPFADoubleBuffer,
-        NSOpenGLPFADepthSize, 24,
-        0
-    };
-    NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attributes];
-    self.openGLView = [[NSOpenGLView alloc] initWithFrame:rect pixelFormat:pixelFormat];
     [self.window.contentView addSubview:self.openGLView];
-
     [self.window makeKeyAndOrderFront:nil];
-    NSLog(@"hello");
-    appInit();
 }
 @end
-
-
-@interface ViewController : NSViewController
-@end
-@implementation ViewController
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-}
-@end
-
 
 int main(int argc, char* argv[]) {
     appEntry(argc, argv);
